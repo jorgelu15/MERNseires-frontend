f2deadc56f79132977672af1145c83a8058b37e7
jorgelu15 <deliarosaro112@gmail.com>
2020-07-14 21:10:13 -0500

    Add files via upload

---
+++ build/precache-manifest.ecefee16ec96e96599e6ed36584884db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6750c6ec025bfb81255ea656d4976fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a48e809cadef5d8b76a",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e7cccaf2a4734ba5172d",
+    "url": "/static/css/main.b149c0c1.chunk.css"
+  },
+  {
+    "revision": "9a48e809cadef5d8b76a",
+    "url": "/static/js/2.6c0898cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6c0898cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cccaf2a4734ba5172d",
+    "url": "/static/js/main.66769fe6.chunk.js"
+  },
+  {
+    "revision": "3d65da31dd14f5f86b57",
+    "url": "/static/js/runtime-main.56ca4f11.js"
+  }
+]);
